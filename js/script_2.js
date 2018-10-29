e3d0ba5f7ed60153b946fd99f5bd1673591972fb
Rianne <rianne285@gmail.com>
2018-10-29 13:36:15 +0100

    Microinteractie responsive

--- js/script_2.js
+++ js/script_2.js
@@ -3,26 +3,30 @@
 /*eslint-env browser*/
 
 /*eslint 'no-console': 0*/
+
 /*MICROINTERACTIE 2 - ALLE VERHALEN DOWNLOADEN*/
 var allesDownloaden = document.querySelector('#download');
 var dowButton = document.querySelector('#download');
 var balk = document.querySelector('.pagina div:first-of-type');
 var melding = document.querySelector('#melding');
-var dowImg = document.querySelector('.dl-knop');
-
+var downloadKnoppen = document.querySelectorAll('.dl-knop');
+//console.log("downloadKnoppen", downloadKnoppen);
 
 allesDownloaden.addEventListener('click', function () {
 	dowButton.classList.toggle('show');
 	balk.classList.toggle('show');
 	document.querySelector('.pagina > p').textContent = ("Downloaden. . .");
-	dowImg.src = ("images/download-load.gif");
 
+	for (var i = 0; i < downloadKnoppen.length; i++) {
+		downloadKnoppen[i].src = ("images/download-load.gif");
+	}
 
 	setTimeout(function () {
 		document.querySelector('.pagina > p').textContent = ("Alles gedownload!");
 		melding.classList.add('show');
-		dowImg.src = ("images/download-finished.png");
+
+		for (var i = 0; i < downloadKnoppen.length; i++) {
+			downloadKnoppen[i].src = ("images/download-finished.png");
+		}
 	}, 3000);
 });
-
-console.log(allesDownloaden);
